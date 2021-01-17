2a090df7e31747d8a848a97a6d667afcc5aa4b9a
Tara Bolger <77146850+Tarab693@users.noreply.github.com>
2021-01-17 19:43:12 +0000

    Add files via upload

---
+++ Band_Name_Game.py
@@ -0,0 +1,6 @@
+print('Bored?, looking for a band name quick? then you are in the right place!')
+place = input('What city where you born in?\n')
+pet_name = input('What is the name of your favorite pet?\n')
+print('Your rocking band name is!', "" + place + "", pet_name)
+
+
